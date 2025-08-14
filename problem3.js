c1271da02ed023ea69e1884bd9106190f8c386fe
mjh-shikder <mjhsikder@gmail.com>
2025-08-14 21:07:02 +0600

    problem3 solved

---
+++ problem3.js
@@ -0,0 +1,22 @@
+function bestTeam(player1, player2) {
+    if (typeof player1 !== "object" || typeof player2 !== "object") {
+        return "Invalid";
+    }
+
+    const total1 = player1.foul + player1.cardY + player1.cardR;
+    const total2 = player2.foul + player2.cardY + player2.cardR;
+
+    if (total1 < total2) {
+        return player1.name;
+    }
+    else if (total2 < total1) {
+        return player2.name;
+    }
+    else {
+        return "Tie";
+    }
+}
+
+
+console.log(bestTeam({ name: "Germany", foul: 10, cardY: 1, cardR: 1 },"France"));
+
